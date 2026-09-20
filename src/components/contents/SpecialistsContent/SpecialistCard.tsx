0f5731d5c4e1ecca3ad0b2547e85c3aa2651e6b2
AleksanderGrieger <61835945+AleksanderGrieger@users.noreply.github.com>
2026-09-20 21:14:02 +0200

    SpecialistCards content is drived from json instead of direct from component

--- src/components/contents/SpecialistsContent/SpecialistCard.tsx
+++ src/components/contents/SpecialistsContent/SpecialistCard.tsx
@@ -1,18 +1,26 @@
+import { StaticImageData } from "next/image";
 import styles from "./SpecialistCard.module.scss";
+import { CSSProperties } from "react";
 
-interface SpecialistCardProps {
+export interface SpecialistCardProps {
   name: string;
+  image: StaticImageData;
   profession: string;
   description: string;
 }
 
 export const SpecialistCard = ({
   name,
+  image,
   profession,
   description,
 }: SpecialistCardProps) => {
+  const cardStyle = {
+    "--specialist-img": `url(${image.src})`,
+  } as CSSProperties;
+
   return (
-    <div className={styles.specialistCard}>
+    <div className={styles.specialistCard} style={cardStyle}>
       <div className={styles.infoContainer}>
         <span className={styles.name}>{name}</span>
         <span className={styles.profession}>{profession}</span>
--- src/components/contents/SpecialistsContent/SpecialistsContent.tsx
+++ src/components/contents/SpecialistsContent/SpecialistsContent.tsx
@@ -1,69 +1,21 @@
+import { specialistCards } from "@/src/constants/content-objects";
 import { SpecialistCard } from "./SpecialistCard";
 import styles from "./SpecialistsContent.module.scss";
 
 export const SpecialistsContent = () => {
   return (
-    // todo poprosic o same zdjecia - nie grafiki i fajnie jakby Karolina tez miala na bialym tle
     <>
       <div className="container">
         <div className={styles.cardsWrapper}>
-          <SpecialistCard
-            name="Karolina Bobryk-Kopcińska"
-            profession="ZAWODOWA TANCERKA"
-            description="
-              Tancerka międzynarodowej klasy tanecznej ‚S’ w tańcach
-              latynoamerykańskich. Mistrzyni Polski w formacji tanecznej,
-              półfinalistka Mistrzostw Świata formacji, wielokrotna Mistrzyni
-              Pomorza. Od wielu lat trenerka i instruktorka tańca,
-              specjalizująca się w tańcach latynoamerykańskich oraz karaibskich.
-              Wytrenowała Mistrzynie Polski, Świata i Europy Federacji WADF. Z
-              wykształcenia magister fizjoterapii specjalizująca się w terapii
-              wad w postawie ciała oraz skoliozy oraz terapeutka integracji
-              sensorycznej."
-          />
-          <SpecialistCard
-            name="Hanna Maciejewska"
-            profession="ZAWODOWA TANCERKA"
-            description="
-              Tancerka najwyższej międzynarodowej klasy tanecznej ‚S’ w tańcach
-              standardowych i latynoamerykańskich. Mistrzyni Polski w tańcach
-              standardowych na Otwartych Mistrzostwach Polski, II Vicemistrzyni
-              Polski w 10 tańcach, wielokrotna finalistka Mistrzostw Polski,
-              półfinalistka Pucharu Europy w 10 tańcach. Magister wychowania
-              fizycznego oraz oligofrenopedagog. Instruktor sportu tanecznego,
-              trener personalny oraz trener osób z niepełnosprawnościami."
-          />
-          <SpecialistCard
-            name="Milena Chomicz"
-            profession="ZAWODOWA TANCERKA"
-            description="
-              Instruktor i Choreograf tańca ludowego oraz tańca
-              charakterystycznego. Tancerka z 26 letnim stażem tanecznym.
-              Wychowanka Zespołu Tanecznego „Algi” w Kołobrzegu, w którym
-              tańczyła 15 lat oraz wyjeżdżała przez 10 lat w trasy koncertowe do
-              Danii. W ‚Algach’ zdobywała swoje doświadczenie oraz uczyła
-              kolejne pokolenia tancerzy. Prowadząca zajęcia z dziećmi,
-              młodzieżą oraz dorosłymi. Od 15 lat przygotowuje również polonezy
-              studniówkowe w szkołach średnich. Kocha taniec ludowy, to jej
-              życiowa pasja, miłością darzy też taniec współczesny, towarzyski
-              oraz high heels"
-          />
-          <SpecialistCard
-            name="Magdalena Łopucka"
-            profession="ZAWODOWA TANCERKA"
-            description="
-              Tancerka z 25 letnim stażem. Z wykształcenia choreograf tańca,
-              nauczyciel i trener odżywiania. W swojej karierze wyszkoliła już
-              kilka pokoleń polskich tancerzy, którzy prężnie rozwijają swoje
-              umiejętności na arenie światowej. Wielokrotnie uznawana na
-              szczeblach ogólnopolskich i międzynarodowych, lecz to zadowolenie
-              tancerzy na sali treningowej cieszy ją najbardziej. Specjalizuje
-              się w wielu technikach tańca, w szczególności w tańcu
-              współczesnym, salsie, bachacie i autorskim projekcie POWER DANCE -
-              czyli połączeniu tańca z elementami fitnessowymi, który nie tylko
-              zadowoli miłośników energicznych choreografii, ale również tych,
-              którzy chcą spalać kalorie i wymodelować sylwetkę."
-          />
+          {specialistCards.map((card) => (
+            <SpecialistCard
+              key={card.name}
+              name={card.name}
+              image={card.image}
+              profession={card.profession}
+              description={card.description}
+            />
+          ))}
         </div>
       </div>
     </>
